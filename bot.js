d384d4c7d1d6fb9ff5af9070e1a80b8e14c1c987
iiMohamedYT88 <46201203+iiMohamedYT88@users.noreply.github.com>
2018-12-28 02:16:01 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("515240557874905089")
+setInterval(function() {
+channel.send(``i love iTzSemba i love iTzSembai love iTzSembai love iTzSembai love iTzSembai love iTzSembai love iTzSembai love iTzSembai love iTzSembai love iTzSembai love iTzSemba`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
